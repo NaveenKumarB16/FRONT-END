4da67338e816dbc9598777b51b5b238c67a9477f
Naveen Kumar <naveennicky16.com>
2023-11-14 14:39:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css'
+import SignUp from './Pages/SignUp';
+import Login from './Pages/Login';
+import Home from './Pages/home';
+import {Route, Routes} from "react-router-dom";
+function App() {
+
+  return (
+  <>
+  <Routes>
+    <Route path="/" element={<SignUp/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/Home" element={<Home/>} />
+  </Routes>
+  </>
+ );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Button } from 'react-bootstrap'
+import "../Styles/Home.css"
+
+const Home = () => {
+    return (
+        <Container>
+            <h1>WELCOME TO OUR WEBSITE</h1>
+            <p>We are here to serve you</p>
+            <Button variant="primary" type="submit">
+                Get S+tarted
+            </Button>
+        </Container>
+
+    )
+}
+
+export default Home;
