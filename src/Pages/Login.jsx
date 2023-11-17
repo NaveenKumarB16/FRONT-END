7f29e2f5bc40158bbed7cac1ddfee81f6a925c03
Naveen Kumar <naveennicky16.com>
2023-11-17 10:24:35 +0530

    changes in main.j

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,47 +1,58 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import "../Styles/Login.css";
+import { Link, useNavigate } from "react-router-dom";
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Retrieve users from session storage
-    const users = JSON.parse(sessionStorage.getItem('users')) || [];
+    const handleLogin = () => {
+        // Retrieve users from session storage
+        const users = JSON.parse(sessionStorage.getItem('users')) || [];
+    
+        // Check if the entered credentials match stored credentials
+        const user = users.find((u) => u.username === username && u.password === password);
+    
+        if (user) {
+          console.log('User logged in:', user);
+    
+          // After successful login, navigate to the home page (or dashboard)
+          navigate('/home');
+        } else {
+          alert('Invalid username or password');
+        }
 
-    // Check if the entered credentials match stored credentials
-    const user = users.find((u) => u.username === username && u.password === password);
+    const handleChange = (e) => {
+        const{ name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
-    if (user) {
-      console.log('User logged in:', user);
-
-      // After successful login, navigate to the home page (or dashboard)
-      navigate('/home');
-    } else {
-      alert('Invalid username or password');
-    }
-  };
-
-  return (
-    <div className="container">
-      <h2>Login</h2>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button onClick={handleLogin}>Login</button>
-      <p>
-        Don't have an account? <Link to="/register">Register</Link>
-      </p>
-    </div>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+    return (
+        <Container>
+            <h1>LOGIN</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>EMAIL</Form.Label>
+                    <Form.Control type="email" name="email" value={formData.email} onChange={handleChange} required/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>PASSWORD</Form.Label>
+                    <Form.Control type="password" name="password" value={formData.password} onChange={handleChange} required/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+        </Container>
+        
+    );
 };
 
 export default Login;
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,51 +1,48 @@
-// Try this code Register
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import "../Styles/SignUp.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+    const navigate = useNavigate();
 
-  const handleSignUp = () => {
-    // Check if the user already exists in session storage
-    const existingUsers = JSON.parse(sessionStorage.getItem('users')) || [];
-
-    // Check if the username is already taken
-    if (existingUsers.some((user) => user.username === username)) {
-      alert('Username is already taken. Please choose another one.');
-    } else {
-      // Register the new user
-      const newUser = { username, password };
-      const updatedUsers = [...existingUsers, newUser];
-      sessionStorage.setItem('users', JSON.stringify(updatedUsers));
-
-      console.log('User registered:', newUser);
-
-      // After successful registration, navigate to the login page
-      navigate('/');
-    }
-  };
-
-  return (
-    <div className="container">
-      <h2>Register</h2>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button onClick={handleRegister}>Register</button>
-      <p>
-        Already have an account? <Link to="/">Login</Link>
-      </p>
-    </div>
-  );
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+        const handleSubmit = (e) => {
+            e.preventDefault();
+        console.log(formData);
+        navigate('/');
+    };
+    return(
+        <Container>
+            <h1>Registration Form</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name="name" value={formData.name} onChange={handleChange} required/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>EMAIL</Form.Label>
+                    <Form.Control type="email" name="email" value={formData.email} onChange={handleChange} required/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>PASSWORD</Form.Label>
+                    <Form.Control type="password" name="password" value={formData.password} onChange={handleChange} required/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                <p>already have an account? <Link to="/Login">Login</Link></p>
+            </Form>
+        </Container>
+    );
 };
 
-export default SignUp;
+export default SignUp; 
